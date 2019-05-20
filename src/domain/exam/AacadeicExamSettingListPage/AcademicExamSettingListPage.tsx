5035dc4e2eb157f96b85a0f3d46427cb21b5e6e4
suryasree-reddy <35096385+suryasree-reddy@users.noreply.github.com>
2019-05-20 12:18:36 +0530

    changes in table design

--- src/domain/exam/AacadeicExamSettingListPage/AcademicExamSettingListPage.tsx
+++ src/domain/exam/AacadeicExamSettingListPage/AcademicExamSettingListPage.tsx
@@ -14,9 +14,6 @@ const w180 = {
 const AcademicExamSettingRow = ({ academicExamSetting }: { academicExamSetting: AcademicExamSettingSummaryFragment }) => (
   <tr key={academicExamSetting.id}>
     <td>
-      <input type="checkbox" name="" id="" />
-    </td>
-    <td>
       <Link
         className="table-link link-color"
         to={`/plugins/ems-exam/page/academicExamSetting?id=${academicExamSetting.id}`}
@@ -24,19 +21,18 @@ const AcademicExamSettingRow = ({ academicExamSetting }: { academicExamSetting: 
         {academicExamSetting.examType}
       </Link>
     </td>
-    <td>{academicExamSetting.semester}</td>
-    <td>{academicExamSetting.subject}</td>
-    <td>{academicExamSetting.date}</td>
-    <td>{academicExamSetting.day}</td>
-    <td>{academicExamSetting.duration}</td>
-    <td>{academicExamSetting.startTime}</td>
-    <td>{academicExamSetting.endTime}</td>
-    <td>{academicExamSetting.total}</td>
-    <td>{academicExamSetting.passing}</td>
-    <td>{academicExamSetting.actions}</td>
     <td>{academicExamSetting.department.name}</td>
     <td>{academicExamSetting.academicYear.year}</td>
     <td>{academicExamSetting.section.section}</td>
+    <td>{academicExamSetting.date}</td>
+    <td>{academicExamSetting.date}</td>
+    <td> <a >
+            <i className="fa fa-pencil-square-o fa-1-5x" aria-hidden="true"></i>
+          </a>
+          <a >
+            <i className="fa fa-trash-o fa-1-5x table-p" aria-hidden="true"></i>
+          </a></td>   
+   
   </tr>
 );
 
@@ -45,23 +41,14 @@ const AcademicExamSettingsTable = ({ academicExamSettings }: { academicExamSetti
    <table id="academicexamsettinglistpage" className="striped-table fwidth bg-white">
       <thead>
         <tr>
-          <th>
-            <input type="checkbox" value="checkedall" name="" id="" />
-          </th>
           <th>Exam Type</th>
-          <th>Semester</th>
-          <th>Subject</th>
-          <th>Date</th>
-          <th>Day</th>
-          <th>Duration</th>
-          <th>Start Time</th>
-          <th>End Time</th>
-          <th>Total</th>
-          <th>Passing</th>
-          <th>Actions</th>
           <th>Department</th>
-          <th>Section</th>
           <th>Academic Year</th>
+          <th>Section</th>
+          <th>Start Date</th>
+          <th>End Date</th>
+          <th>Actions</th>
+                   
         </tr>
       </thead>
       <tbody>
@@ -80,15 +67,15 @@ const AcademicExamSettingListPage = ({ data: { academicExamSettings } }: Academi
   <section className="customCss">
     <h3 className="bg-heading p-1 m-b-0">
       <i className="fa fa-university stroke-transparent mr-1" aria-hidden="true" />{' '}
-      Admin - AcademicExamSetting Management
+      Admin - Academic Exam Setting Management
       </h3>
     <div className="plugin-bg-white p-1">
-      <div className="m-b-1 dflex bg-heading">
-        <h4 className="ptl-06">AcademicExamSetting Details</h4>
+      <div className="m-b-1 eflex bg-heading">
+        <h4 className="ptl-06">Academic Year</h4>
         <div>
           <Link
             to={`/plugins/ems-exam/page/addacademicExamSetting`}
-            className="btn btn-primary m-r-1" style={w180}>Create New AcademicExamSetting
+            className="btn btn-primary m-r-1" style={w180}>Create New Exam
         </Link>
           <a className="btn btn-primary">Export</a>
         </div>
